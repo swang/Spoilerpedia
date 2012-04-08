4a306782f51722c1716ae4feb9c1bb7cd3a6ff82
Shuan Wang <shuanwang@gmail.com>
2012-04-08 03:45:44 -0700

    Minor cosmetic changes to coding style

--- spoilerpedia.js
+++ spoilerpedia.js
@@ -11,6 +11,7 @@ spoilerpedia.youtube = function() {
           videosDivWidth = document.querySelector("div#spoilerpedia-videos").clientWidth
           videos = videosDiv.querySelectorAll("iframe"),
           videoWidth = Math.floor(videosDivWidth / 3) - 20
+
       for ( var i = 0; i < videos.length; i++)
       {
         videos[i].setAttribute('width', videoWidth)
@@ -23,13 +24,12 @@ spoilerpedia.youtube = function() {
      * Generates an iframe element with the YouTube video with id, 'videoId' and a width of 'width'
      */
     generateYouTubeVideo: function(videoId, width) {
-
       var vidIframe = document.createElement("iframe")
 
       vidIframe.setAttribute("width", width)
       vidIframe.setAttribute("height", Math.floor( width / (560/315.0)))
       vidIframe.setAttribute("frameborder", 0)
-      vidIframe.setAttribute("src", "https://www.youtube.com/embed/"+ videoId) //4G1xO_B2V7s")
+      vidIframe.setAttribute("src", "https://www.youtube.com/embed/"+ videoId) //4G1xO_B2V7s
 
       return vidIframe
     },
@@ -67,13 +67,13 @@ spoilerpedia.wiki = function() {
      * we will search for that in its list of categories.
      */
     wikiPageIsAFilm: function() {
-
       var matchYearFilm = /[0-9]{4} Films/i,
           cats = target.getElementById('mw-normal-catlinks').getElementsByTagName("li"),
           isFilm = false
 
       for (var i = 0; i < cats.length; i++) {
         var aLink = cats[i].querySelector("a") //getElementsByTagName("a")[0]
+
         if (matchYearFilm.test(aLink.innerText)) {
           isFilm = true
         }
@@ -87,6 +87,7 @@ spoilerpedia.wiki = function() {
      */
     findHeadline: function(headlineText) {
       var headlines = target.getElementsByClassName('mw-headline')
+      
       for (var i = 0; i < headlines.length; i++) {
         if (new RegExp("^" + headlineText , "i").test( headlines[i].innerText )) {
           return headlines[i]
@@ -116,6 +117,7 @@ spoilerpedia.wiki = function() {
       var parentPlotNode = plotHeadline.parentNode,
           sibl = parentPlotNode.nextElementSibling,
           para
+            
       while ( sibl && sibl.tagName.toString() != parentPlotNode.tagName.toString()) {
         if (sibl.tagName == "P") {
           para = sibl
@@ -132,9 +134,12 @@ spoilerpedia.wiki = function() {
     applySpoiler: function(paragraph) {
       if (paragraph) {
         var linkStyle = document.defaultView.getComputedStyle( document.getElementsByTagName("a")[0], "")
-        paragraph.className += " spoiler"
+        
+          paragraph.className += " spoiler"
         paragraph.onmouseover  = function() { spoilerpedia.wiki.changeLinkColor(paragraph, linkStyle.getPropertyValue("color"), linkStyle.getPropertyValue("background-color")) }
         paragraph.onmouseout   = function() { spoilerpedia.wiki.changeLinkColor(paragraph, "black", "black") }
+        
+        // set link color in spoiler to black/black
         spoilerpedia.wiki.changeLinkColor(paragraph, "black", "black")
       }
     },
@@ -145,12 +150,15 @@ spoilerpedia.wiki = function() {
     removeSpoiler: function(paragraph) {
       if (paragraph) {
         var linkStyle = document.defaultView.getComputedStyle( document.getElementsByTagName("a")[0], "")
+        
         // remove spoiler class and trims end spaces
         paragraph.className = paragraph.className.replace(/\bspoiler\b/gi,"").replace(/\s\s*$/,"").replace(/^\s\s*/,"") 
         // remove mouseover/out events
         paragraph.onmouseover = null
         paragraph.onmouseout = null
-        spoilerpedia.wiki.changeLinkColor(paragraph,  linkStyle.getPropertyValue("color"), linkStyle.getPropertyValue("background-color"))
+
+        // fix link color to the default, just in case it was stuck on black/black
+        spoilerpedia.wiki.changeLinkColor(paragraph, linkStyle.getPropertyValue("color"), linkStyle.getPropertyValue("background-color"))
       }
     },
 
@@ -161,6 +169,7 @@ spoilerpedia.wiki = function() {
      */
     getSheet: function() {
       var styleSheet = document.querySelector("style#spoilerpedia-css")
+
       if (!styleSheet) {
         styleSheet = document.createElement("style")
         styleSheet.id = "spoilerpedia-css"
@@ -175,6 +184,7 @@ spoilerpedia.wiki = function() {
     getTitle: function() {
       var getSpans = document.getElementById("firstHeading").getElementsByTagName("span"),
           moviePage
+
       for (var i = 0 ; i < getSpans.length; i++) {
         if (getSpans[i].getAttribute("class") != "editsection") {
           moviePage = getSpans[i].innerText
@@ -193,15 +203,12 @@ spoilerpedia.wiki = function() {
           videoDivWidth = this.findHeadline('Plot').parentNode.parentNode.clientWidth,
           movieName = moviePage.replace(/\s+\(.*\)/,''), // returns title of page w/out disambig 
           movieYearInPageName = moviePage.match(/\(([0-9]{4}) .*\)/i), // grabs the year if it exists in disambig
-          
           queryMovie = movieName + (movieYearInPageName && movieYearInPageName.length >= 2 ? (" " + movieYearInPageName[1]) : "") + " ending",
           videoFeed = spoilerpedia.youtube.getYouTubeFeed(queryMovie) // grabs the YouTube feed
           
       videosDiv.id = "spoilerpedia-videos"
-      
       videosDiv.style.width = "auto"
       videosDiv.style.height = "auto"
-
       videosDiv.style.overflow = "hidden"
 
       for ( var i = 0; i < videoFeed.entry.length; i++)
@@ -215,6 +222,7 @@ spoilerpedia.wiki = function() {
     removeSpoilerpedia: function() {
       var plotHeadline = this.findHeadline('Plot'),
           para = this.getLastParagraph(plotHeadline)
+
       this.removeSpoiler(para)
       document.querySelector("div#spoilerpedia-videos").style.display = 'none'
     },
@@ -222,6 +230,7 @@ spoilerpedia.wiki = function() {
     restoreSpoilerpedia: function() {
       var plotHeadline = this.findHeadline('Plot'),
           para = this.getLastParagraph(plotHeadline)
+            
       this.applySpoiler(para)
       document.querySelector("div#spoilerpedia-videos").style.display = 'block'
     },
@@ -229,9 +238,7 @@ spoilerpedia.wiki = function() {
       var plotHeadline = this.findHeadline('Plot')
 
       if ( this.wikiPageIsAFilm() && plotHeadline != null) {
-        
         chrome.extension.sendRequest({"showSpoilerpediaIcon":true}, function response() {})
-
         var moviePage = this.getTitle(),
             para = this.getLastParagraph(plotHeadline)
         
@@ -239,8 +246,8 @@ spoilerpedia.wiki = function() {
 
         this.getSheet().innerHTML = "p.spoiler, p.spoiler > a { background-color: black; color: black; }\np.spoiler:hover { background-color: white; color: black; }\ndiv#spoilerpedia-videos > iframe { padding:10px }\n"
         if (para) {
-
           var videosDiv = this.addVideos(moviePage)
+
           this.applySpoiler(para)
           target.getElementById("mw-content-text").insertBefore(videosDiv , para.nextElementSibling) 
         }
